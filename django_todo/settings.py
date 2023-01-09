8dab80e2190bbb35f596a3b754e80408eb827084
Jean de Villiers <jeandevilliers@me.com>
2023-01-09 16:44:33 +0000

    Update ALLOWED_HOSTS in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -39,7 +39,7 @@ if development:
     ALLOWED_HOSTS = ['localhost']
 else:
     # ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
-    ALLOWED_HOSTS = 'hello-django-todo.herokuapp.com'
+    ALLOWED_HOSTS = '.herokuapp.com'
 
 
 # Application definition
