1a3306367167f8c5ab6a16b2bf07e7d6e1b544b6
Jean de Villiers <jeandevilliers@me.com>
2023-01-09 14:53:16 +0000

    Add ALLOWED_HOSTS='*' to settings.py

--- django_todo/ci-settings.py
+++ django_todo/ci-settings.py
@@ -31,7 +31,8 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    # ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = '*'
 
 
 # Application definition
